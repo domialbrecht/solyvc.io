3d0462823acc1498b517d305cfb099ca750e871e
dominika <dominik.albrecht@intersim.ch>
2024-06-20 16:03:26 +0200

    Setup

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+	import '../app.css';
+	import Header from './Header.svelte';
+</script>
+
+<div class="app">
+	<Header></Header>
+	<main>
+		<slot></slot>
+	</main>
+	<footer>
+		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+	</footer>
+</div>
+<slot></slot>
+
