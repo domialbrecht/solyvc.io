5bdf4286cce98a9cd51270ae4a4fd50f4cbff17a
dominika <dominik.albrecht@intersim.ch>
2024-06-25 15:45:40 +0200

    Add mapbox

---
+++ src/lib/components/Map.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { env } from '$env/dynamic/private';
+	import { onMount } from 'svelte';
+	import 'mapbox-gl/dist/mapbox-gl.css';
+	import mapboxgl from 'mapbox-gl';
+
+	mapboxgl.accessToken = env.MAPBOX_TOKEN;
+
+	let mapElement: HTMLDivElement;
+	let map: mapboxgl.Map;
+
+	onMount(() => {
+		map = new mapboxgl.Map({
+			container: mapElement,
+			style: 'mapbox://styles/mapbox/streets-v11',
+			center: [0, 0],
+			zoom: 1
+		});
+	});
+</script>
+
+<div id="map" bind:this={mapElement}></div>
